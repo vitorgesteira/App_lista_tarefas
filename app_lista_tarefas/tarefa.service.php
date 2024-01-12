95562c098bb3d42a531a6549be272b6cdd924b7e
Vitor Gesteira <dev.vitorgesteira@gmail.com>
2024-01-12 10:34:09 -0300

    Criando a classe TarefaService

---
+++ app_lista_tarefas/tarefa.service.php
@@ -0,0 +1,20 @@
+<?php
+    //CRUD
+    class TarefaService{
+        public function inserir(){ //create
+
+        }
+
+        public function recuperar(){ //read
+
+        }
+
+        public function atualizar(){ //update
+        
+        }
+
+        public function remover(){ //delete
+
+        }
+    }
+?>
