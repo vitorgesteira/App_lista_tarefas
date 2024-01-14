0905aa804427ba2f5e0736423df74ca87eec9f5c
Vitor Gesteira <dev.vitorgesteira@gmail.com>
2024-01-14 13:16:58 -0300

    Inserindo registros (feedback)

--- app_lista_tarefas/tarefa_controller.php
+++ app_lista_tarefas/tarefa_controller.php
@@ -2,10 +2,6 @@
     require "./app_lista_tarefas/tarefa.model.php";
 	require "./app_lista_tarefas/tarefa.service.php";
 	require "./app_lista_tarefas/conexao.php";
-
-	echo '<pre>';
-	print_r($_POST);
-	echo '</pre>';
 	
 	$tarefa = new Tarefa();
 	$tarefa->__set('tarefa', $_POST['tarefa']);
@@ -15,7 +11,5 @@
 	$tarefaService = new TarefaService($conexao, $tarefa);
 	$tarefaService->inserir();
 
-	echo '<pre>';
-	print_r($tarefaService);
-	echo '</pre>';
+	header('Location: nova_tarefa.php?inclusao=1');
 ?>
