4b078612aab33aa8694c352b2e113fa665975774
Vitor Gesteira <dev.vitorgesteira@gmail.com>
2024-01-14 11:24:30 -0300

    Inserindo registros

--- app_lista_tarefas/tarefa.service.php
+++ app_lista_tarefas/tarefa.service.php
@@ -1,8 +1,20 @@
 <?php
     //CRUD
     class TarefaService{
-        public function inserir(){ //create
 
+        private $conexao;
+        private $tarefa;
+
+        public function __construct(Conexao $conexao, Tarefa $tarefa){
+            $this->conexao = $conexao->conectar();
+            $this->tarefa = $tarefa;
+        }
+
+        public function inserir(){ //create
+            $query = 'insert into tb_tarefas(tarefa)value(:tarefa)';
+            $stm = $this->conexao->prepare($query);
+            $stm->bindValue(':tarefa', $this->tarefa->__get('tarefa'));
+            $stm->execute();
         }
 
         public function recuperar(){ //read
